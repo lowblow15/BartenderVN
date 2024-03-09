c078eff79dae4f239d97516ef5d76b9351589ef2
Luke Lowe <luke.lowe68@yahoo.com>
2024-03-09 00:47:06 -0500

    lots of stuff done idfk

--- BartenderVN.cpp
+++ BartenderVN.cpp
@@ -31,6 +31,24 @@ public:
 		olc::vf2d pos;
 		olc::vf2d size;
 	};
+	struct ScreenWipe
+	{
+		Rect box;
+		olc::Pixel Color;
+		float speed;
+		bool rising;
+		bool active;
+		void setScreenWipe(float x,float y)
+		{
+			box.pos = olc::vf2d(x, y);
+			box.size = { 25,25 };
+			Color = olc::WHITE;
+			speed = 500;
+			rising = true;
+			active = true;
+		}
+
+	};
 	struct mapTiles
 	{
 		Rect tileRect;
@@ -146,26 +164,31 @@ public:
 		{
 			StringPos = { boxInner.pos.x + 5,boxInner.pos.y + 5 };
 		}
-		void setSelections(std::vector<std::string> theseStrings, int numofSelections)
+		void setSelections( int numofSelections)
 		{
 			int x = 0;
-			selectionStrings.clear();
-			selectionStrings = theseStrings;
+			
 			Selections.clear();
 			Selections.resize(numofSelections);
 			selectionStringPos.clear();
 			selectionStringPos.resize(numofSelections);
 			for (auto i = Selections.begin(); i < Selections.end(); i++)
 			{
-				i->box.pos = { boxInner.pos.x + 10,boxInner.pos.y + 10 + 10 * x };
+				i->box.pos = { boxInner.pos.x + 10,boxInner.pos.y + 10 + 35 * x };
+				i->box.size = { 25,25 };
+				i->innerBox.pos = { i->box.pos.x+2,i->box.pos.y+2 };
+				i->innerBox.size = { i->box.size.x - 4,i->box.size.y - 4 };
+				i->buttontitle = selectionStrings[x];
 				x++;
 			}
 			x = 0;
 			for (auto i = selectionStringPos.begin(); i < selectionStringPos.end(); i++)
 			{
 				*i = { Selections[x].box.pos.x + Selections[x].box.size.x + 10,Selections[x].box.pos.y + Selections[x].box.size.y / 2 };
+				Selections[x].stringPos = *i;
 				x++;
 			}
+			Selections[0].checked = true;
 		}
 	};
 	struct cutScene
@@ -175,6 +198,7 @@ public:
 		std::vector<std::string> MainCharStrings;
 		int talkState= 0;
 		bool Over = false;
+		bool lastNPCLine;
 		int numOfLines = 0;
 		void toggleTalkState()
 		{
@@ -188,29 +212,43 @@ public:
 			}
 		}
 		void NextStringSetter()
-		{
-			toggleTalkState();
-			switch (talkState)
+		{ 
+			if (Over == false)
 			{
-			case 0:
-				if (numOfLines >= 0)
-				{
 
-					numOfLines--;
-					setNextCharacterString(MainCharStrings[0]);
-					MainCharStrings.erase(MainCharStrings.begin());
-				}
-				else
+
+				toggleTalkState();
+				switch (talkState)
 				{
-					ConversationUpdater();
+				case 0:
+					if (numOfLines > 1)
+					{
+
+						numOfLines--;
+						setNextCharacterString(MainCharStrings[0]);
+						MainCharStrings.erase(MainCharStrings.begin());
+					}
+					else
+					{
+						ConversationUpdater();
+						lastNPCLine = true;
+					}
+					break;
+				case 1:
+					if (npcStrings.size() < 1)
+					{
+						bool breaker = true;
+					}
+					setNextNpcString(npcStrings[0]);
+					npcStrings.erase(npcStrings.begin());
+					if (lastNPCLine == true)
+					{
+						Over = true;
+					}
+					break;
+				default:
+					break;
 				}
-				break;
-			case 1:
-				setNextNpcString(npcStrings[0]);
-				npcStrings.erase(npcStrings.begin());
-				break;
-			default:
-				break;
 			}
 		}
 		void setNextNpcString(std::string thisString)
@@ -238,20 +276,23 @@ public:
 	int nextNpcNum = 0;
 	int nextSceneNum = 0;
 	bool gameOver = false;
+	ScreenWipe Wiper;
+	bool ScreenWiping = false;
 	std::vector<std::vector<cutScene>> Scenes;
 	cutScene* CurrentScenePtr;
 	npc* ActiveNPC;
 	std::vector<std::string> Drinks = {"Manhattan","Martini","Red Wine","Scotch and Soda","Daiquiri","Old Fashioned"};
 	std::vector<std::vector<std::string>> Problems;
 	std::vector<std::string> sprStrings;
-
-
+	std::vector<std::string> ChangelingSprStrings;
+	int SelectionNum = 0;
+	bool SettingNextScene = false;
 	std::vector<int> npcNums = { drifter,officelady,trucker,marriedwoman};
 public:
 	void parseFile( int npcnum, int scenenum, const std::string& filename, std::vector<std::string>& TheseStrings)
 	{
 		TheseStrings.clear();
-
+		std::string tokenToCheck = { std::to_string(npcnum) + " " + std::to_string(scenenum) };
 		std::ifstream file(filename);
 		/*if (!file.is_open()) {
 			std::cerr << "Error opening file: " << filename << std::endl;
@@ -263,29 +304,92 @@ public:
 
 			std::string line;
 			while (std::getline(file, line)) {
-				std::istringstream iss(line);
+				if (line.find(tokenToCheck) != std::string::npos)
+				{
+					//std::istringstream iss(line);
 
-				std::string text;
+					std::string text;
 
-				// Parse NPC number, scene number, and text
-				if (iss >> npcnum >> scenenum) {
-					// Read the rest of the line as text
-					text = line.substr(line.find(" ") + 1);
+					// Parse NPC number, scene number, and text
+				
+						// Read the rest of the line as text
+						text = line.substr(line.find(" ") + 3);
 
-					TheseStrings.emplace_back(text);
+						TheseStrings.emplace_back(text);
 
+					
 				}
 			}
 		}
 		file.close();
 	}
+	void SelectionNumUpdater(bool increase)
+	{
+		if (increase == true)
+		{
+			if (SelectionNum<1)
+			{
+				SelectionNum++;
+			}
+			else
+			{
+				SelectionNum = 0;
+			}
+		}
+		else
+		{
+			if (SelectionNum>0)
+			{
+				SelectionNum--;
+			}
+			else
+			{
+				SelectionNum = 1;
+			}
+		}
+		clearSelected();
+		CurrentScenePtr->SceneWindow.Selections[SelectionNum].checked = true;
+	}
+	void clearSelected()
+	{
+		for (auto i = CurrentScenePtr->SceneWindow.Selections.begin(); i < CurrentScenePtr->SceneWindow.Selections.end(); i++)
+		{
+			i->checked = false;
+		}
+	}
+	
 	void DrawCutscene()
 	{
 		DrawRectDecal(CurrentScenePtr->SceneWindow.boxOuter.pos, CurrentScenePtr->SceneWindow.boxOuter.size);
 		FillRectDecal(CurrentScenePtr->SceneWindow.boxInner.pos, CurrentScenePtr->SceneWindow.boxInner.size,olc::BLUE);
-		DrawStringDecal(CurrentScenePtr->SceneWindow.StringPos, CurrentScenePtr->SceneWindow.displayString);
+		if (CurrentScenePtr->SceneWindow.selectionMode == false)
+		{
+			DrawStringDecal(CurrentScenePtr->SceneWindow.StringPos, CurrentScenePtr->SceneWindow.displayString);
+		}
+		else
+		{
+			for (auto i = CurrentScenePtr->SceneWindow.Selections.begin(); i < CurrentScenePtr->SceneWindow.Selections.end(); i++)
+			{
+				DrawSelectionBox(*i);
+			}
+		}
+		
 		/*DrawDecal(CurrentScenePtr->SceneWindow.MainChar->PortraitPos, CurrentScenePtr->SceneWindow.MainChar->PortraitDecptr);
 		DrawDecal(CurrentScenePtr->SceneWindow.NpcInScene->PortraitPos, CurrentScenePtr->SceneWindow.NpcInScene->PortraitDecptr);*/
+	}
+	void DrawSelectionBox(checkBox& thisBox)
+	{
+		DrawRectDecal(thisBox.box.pos, thisBox.box.size);
+		if (thisBox.checked == false)
+		{
+			FillRectDecal(thisBox.innerBox.pos, thisBox.innerBox.size, olc::Pixel(0, 0, 0, 200));
+		}
+		else
+		{
+			FillRectDecal(thisBox.innerBox.pos, thisBox.innerBox.size, olc::Pixel(255,215, 0, 200));
+		}
+		DrawStringDecal(thisBox.stringPos, thisBox.buttontitle);
+
 	}
 	void DayManager()
 	{
@@ -315,15 +419,21 @@ public:
 		{
 			Scenes[i].erase(Scenes[i].begin());
 		}
+		SetCurrentScene(nextNpcNum, 0);
 	}
 	void SetCurrentScene(int npcnum , int scenenum)
 	{
 		CurrentScenePtr = &Scenes[npcnum][scenenum];
+		CurrentScenePtr->NextStringSetter();
 	}
 	void PickRandomNPC()
 	{
 		int randint = rand() % NPCs.size() - 1;
-		ActiveNPC = &NPCs[randint];
+		NPCs[randint].Changling = true;
+		NPCs[randint].PortraitsprPtr = new olc::Sprite(ChangelingSprStrings[randint]);
+		NPCs[randint].PortraitDecptr = new olc::Decal(NPCs[randint].PortraitsprPtr);
+		//ActiveNPC = &NPCs[randint];
+
 
 	}
 
@@ -343,22 +453,32 @@ public:
 	    scenenum = 0;
 		for (auto i = Scenes.begin(); i < Scenes.end(); i++)
 		{
+			if (scenenum ==2)
+			{
+				PickRandomNPC();
+			}
 			for (auto x = i->begin(); x < i->end(); x++)
 			{
 				
 				x->SceneWindow.NpcInScene = &NPCs[npC];
 				x->SceneWindow.MainChar = &MainCharacter;
-				/*parseFile(npC, scenenum, "./BartenderVN/assets/Story.txt", x->npcStrings);
-				parseFile(npC, scenenum, "./BartenderVN/assets/BartenderReply.txt", x->MainCharStrings);
-				parseFile(npC, scenenum, "./BartenderVN/assets/Choices.txt", x->SceneWindow.selectionStrings);*/
-				parseFile(npC, scenenum, "./assets/Story.txt", x->npcStrings);
-				parseFile(npC, scenenum, "./assets/BartenderReply.txt", x->MainCharStrings);
-				parseFile(npC, scenenum, "./assets/Choices.txt", x->SceneWindow.selectionStrings);
+				if (x->SceneWindow.NpcInScene->Changling==false)
+				{
+					parseFile(npC, scenenum, "./assets/Story.txt", x->npcStrings);
+					parseFile(npC, scenenum, "./assets/BartenderReply.txt", x->MainCharStrings);
+					parseFile(npC, scenenum, "./assets/Choices.txt", x->SceneWindow.selectionStrings);
+				}
+				else
+				{
+					parseFile(4, scenenum, "./assets/Story.txt", x->npcStrings);
+					parseFile(4, scenenum, "./assets/BartenderReply.txt", x->MainCharStrings);
+					parseFile(4, scenenum, "./assets/Choices.txt", x->SceneWindow.selectionStrings);
+				}
+				x->SetNumOfLines();
+				//x->NextStringSetter();
 
-				x->NextStringSetter();
-
-				x->SceneWindow.setBox(olc::vf2d( 5,ScreenHeight() - 150 ), olc::vf2d(  ScreenWidth() - 15, 125 ), olc::BLUE, olc::WHITE);
-				
+				x->SceneWindow.setBox(olc::vf2d( 5,ScreenHeight() - 150 ), olc::vf2d(  ScreenWidth() - 15, 75 ), olc::BLUE, olc::WHITE);
+				x->SceneWindow.setStringPos();
 			}
 			npC++;
 			scenenum++;
@@ -392,6 +512,7 @@ public:
 			InitCutscenes(npcNums[i], 7);
 		}
 		SetCurrentScene(nextNpcNum, nextSceneNum);
+		CurrentScenePtr->SceneWindow.setSelections( 2);
 	}
 	void UpdatenextnpcNum()
 	{
@@ -402,20 +523,18 @@ public:
 		else
 		{
 			nextNpcNum = 0;
-			UpdateNextSceneNum();
+			
 		}
 	}
-	void UpdateNextSceneNum()
+	void ConfirmSelection()
 	{
-		if (nextSceneNum < Scenes[0].size())
+		if (SelectionNum == 0)
 		{
-			nextSceneNum++;
-			GameStateManager(EODstate);
+			score += 15;
 		}
-		else
-		{
-			gameOver = true;
-		}
+		CurrentScenePtr->SceneWindow.selectionMode = false;
+		CurrentScenePtr->NextStringSetter();
+
 	}
 	void GameStateManager(int newState)
 	{
@@ -426,25 +545,45 @@ public:
 		switch (GameState)
 		{
 		case cutsceneState:
-			if (GetKey(olc::Key::RIGHT).bReleased||GetKey(olc::Key::D).bReleased)
+			if (ScreenWiping == false)
 			{
 
-			}
-			if (GetKey(olc::Key::DOWN).bReleased || GetKey(olc::Key::S).bReleased)
-			{
 
-			}
-			if (GetKey(olc::Key::LEFT).bReleased || GetKey(olc::Key::A).bReleased)
-			{
+				if (CurrentScenePtr->SceneWindow.selectionMode == true)
+				{
 
-			}
-			if (GetKey(olc::Key::UP).bReleased || GetKey(olc::Key::W).bReleased)
-			{
 
-			}
-			if (GetKey(olc::Key::ENTER).bReleased)
-			{
+					if (GetKey(olc::Key::RIGHT).bReleased || GetKey(olc::Key::D).bReleased)
+					{
+						SelectionNumUpdater(true);
+					}
+					if (GetKey(olc::Key::DOWN).bReleased || GetKey(olc::Key::S).bReleased)
+					{
+						SelectionNumUpdater(true);
+					}
+					if (GetKey(olc::Key::LEFT).bReleased || GetKey(olc::Key::A).bReleased)
+					{
+						SelectionNumUpdater(false);
+					}
+					if (GetKey(olc::Key::UP).bReleased || GetKey(olc::Key::W).bReleased)
+					{
+						SelectionNumUpdater(false);
+					}
 
+					if (GetKey(olc::Key::ENTER).bReleased)
+					{
+						ConfirmSelection();
+					}
+				}
+				else
+				{
+
+
+					if (GetKey(olc::Key::ENTER).bReleased)
+					{
+						CurrentScenePtr->NextStringSetter();
+					}
+				}
 			}
 			break;
 		case barState:
@@ -503,12 +642,78 @@ public:
 	{
 
 	}
+	void CutsceneUpdater(float fElapsedTime)
+	{
+		if (CurrentScenePtr->Over ==true||ScreenWiping == true)
+		{
+			if (ScreenWiping == false)
+			{
+				Wiper.setScreenWipe(ScreenWidth() / 2, ScreenHeight() / 2);
+				ScreenWiping = true;
+
+			}
+			else
+			{
+				UpdateScreenWipe(fElapsedTime);
+				if (Wiper.rising ==false&&SettingNextScene ==false)
+				{
+					SettingNextScene = true;
+					UpdatenextnpcNum();
+					SetCurrentScene(nextNpcNum, 0);
+				}
+				if (Wiper.active == false)
+				{
+					ScreenWiping = false;
+					SettingNextScene = false;
+				}
+			}
+		}
+	}
+	void UpdateScreenWipe(float fElapsedTime)
+	{
+		if (Wiper.rising ==true)
+		{
+			if (Wiper.box.size.x <ScreenWidth()+300)
+			{
+				Wiper.box.size.x += Wiper.speed * fElapsedTime;
+				Wiper.box.size.y += Wiper.speed * fElapsedTime;
+				Wiper.box.pos.x -= Wiper.speed/2 * fElapsedTime;
+				Wiper.box.pos.y -= Wiper.speed/2 * fElapsedTime;
+			}
+			else
+			{
+				Wiper.rising = false;
+			}
+		}
+		else
+		{
+			if (Wiper.box.size.y>0)
+			{
+				Wiper.box.size.x -= Wiper.speed * fElapsedTime;
+				Wiper.box.size.y -= Wiper.speed * fElapsedTime;
+				Wiper.box.pos.x += Wiper.speed/2 * fElapsedTime;
+				Wiper.box.pos.y += Wiper.speed/2 * fElapsedTime;
+			}
+			else
+			{
+				Wiper.active = false;
+			}
+		}
+	}
+	void DrawScreenWipe()
+	{
+		FillRectDecal(Wiper.box.pos, Wiper.box.size, Wiper.Color);
+	}
 	void DrawScreen(float fElapsedTime)
 	{
 		switch (GameState)
 		{
 		case cutsceneState:
 			DrawCutscene();
+			if (ScreenWiping == true)
+			{
+				DrawScreenWipe();
+			}
 			break;
 		case barState:
 			DrawBar();
@@ -537,7 +742,7 @@ public:
 		DrawScreen(fElapsedTime);
 		if (GameState != EODstate)
 		{
-
+			CutsceneUpdater(fElapsedTime);
 			
 		}
 		else
@@ -566,7 +771,7 @@ int main()
 {
 	srand(time(0));
 	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
+	if (demo.Construct(600, 400, 4, 4))
 		demo.Start();
 
 	return 0;
